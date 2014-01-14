4976cb08ee2cfcb35dfdc535d4710eb4c0c5eb53
Mark Cicoria <mark.cicoria@ayloo.net>
2014-01-13 20:15:05 -0800

    removing node modules

--- node_modules/node-avro-io/example_reader.js
+++
@@ -1,5 +0,0 @@
-require("./index").DataFile.AvroFile()
-	.open('test.avro', null, { flags: 'r' })
-		.on('data', function(data) {
-	  		console.log(data);
-		});
--- node_modules/node-avro-io/index.js
+++
@@ -1,5 +0,0 @@
-var avro = exports;
-
-avro.DataFile = require("./lib/datafile");
-avro.IO = require("./lib/io");
-avro.Schema = require("./lib/schema");
